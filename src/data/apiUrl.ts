ea6e37d5759170ad15c9de76865defe7e0ffed6c
Topitops69 <topitops123@gmail.com>
2024-06-07 12:27:30 +0800

    Change URL for Deployment

--- src/data/apiUrl.ts
+++ src/data/apiUrl.ts
@@ -1,1 +1,3 @@
-export const API_BASE_URL = 'http://localhost:8080/api/v1'
+// export const API_BASE_URL = 'https://insights-blog-production.up.railway.app/api/v1'
+export const API_BASE_URL =
+  'https://insights-blog-production.up.railway.app/api/v1'
