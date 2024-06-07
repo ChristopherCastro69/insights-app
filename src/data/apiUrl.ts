7d606cbc5ad831505289d0abf0a36e931132e675
Topitops69 <topitops123@gmail.com>
2024-06-07 12:56:19 +0800

    Vercel

--- src/data/apiUrl.ts
+++ src/data/apiUrl.ts
@@ -1,3 +1,5 @@
 // export const API_BASE_URL = 'https://insights-blog-production.up.railway.app/api/v1'
 export const API_BASE_URL =
   'https://insights-blog-production.up.railway.app/api/v1'
+
+//test
